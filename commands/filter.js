933eaf172323090e4aa619ede3b01b4bc4eefe0a
Olympicene <kevinjcord@gmail.com>
2021-11-27 14:01:22 -0600

    added filter command, with basic sender react and unsent filtering

---
+++ commands/filter.js
@@ -0,0 +1,73 @@
+import config from 'conf';
+import chalk from 'chalk';
+import * as fs from 'fs';
+import { Reader, listOfFiles } from '../helper/read.js';
+import { dir } from 'console';
+import utf8 from 'utf8';
+import buffer from 'buffer';
+import assert from 'assert';
+import iconv from 'iconv-lite';
+import { urlToHttpOptions } from 'url';
+
+export function filter(option) {
+  ///////////////////////////////////////////////////////////////////////////File Prep
+  //final filter file
+  let filterFile = {
+    participants: [],
+    messages: [],
+  };
+
+  //get all messenger archive files
+  let allFiles = listOfFiles();
+  console.log(`Found ${allFiles.length} messenger archive files.`);
+
+  ///////////////////////////////////////////////////////////////////////////Get Participants + Messages
+  //iterate through all files and get all participants + Messages
+  for (let fileName of allFiles) {
+    let file = new Reader(fileName);
+
+    //go through every message in file
+    for (let message of file.getMessages()) {
+
+      //check if sender filter is present and if message is from sender
+      if (option.sender && !(message.sender_name == option.sender)) {
+        continue;
+      }
+
+      //check if sender filter is present and if message is has exact reacts
+      if (option.reacts && !('reactions' in message && Object.keys(message.reactions).length == option.reacts)) {
+        continue;
+      }      
+      
+      //check if isUnsent filter is present and if message is unsent
+      if ('isUnsent' in option && !(message.is_unsent == option.isUnsent)) {
+        continue;
+      }
+
+      //push message
+      filterFile.messages.push(message); 
+
+      //push participant
+      if (!filterFile.participants.includes(message.sender_name)) { 
+        filterFile.participants.push(message.sender_name);
+      }
+    }
+
+    console.log(`Successfully read ${fileName}`);
+  }
+
+  ///////////////////////////////////////////////////////////////////////////Writing File
+
+  let fileName = option.output ? option.output : 'filter.json';
+
+  //delete if duplicates
+  if(fs.existsSync(fileName)) {
+    console.log('Duplicate File found. Deleting ...');
+    fs.unlinkSync(fileName);
+  }
+
+  fs.writeFileSync(fileName, JSON.stringify(filterFile, null, '\t'));
+  console.log(
+    `Successfully filtered all messenger archive files into ${fileName}`
+  );
+}
--- index.js
+++ index.js
@@ -4,19 +4,29 @@
 import program from 'commander';
 import {collate} from './commands/collate.js';
 import {fixEncoding} from './commands/fixEncoding.js'
+import { filter } from './commands/filter.js';
 import chalk from 'chalk';
 
+
+program
+    .command('fix-encoding')
+    .description(`Fixes Mojibake issue of messenger archive files. ${chalk.red('ONLY RUN ONCE.')} By default will go through every valid file.`)
+    .action(fixEncoding);
+
+program
+    .command('filter')
+    .description(`Filter messages by a variety of properties and outputs to json. By default will go through every valid file.`)
+    .option('-o, --output <file>', 'Name of file to be outputed. If not specified, will default to \'filter.json\'')
+    .option('-s, --sender <name>', 'Filter messages by sender name.')
+    .option('-r, --reacts <value>', 'Filter messages by the number of reactions message received.')
+    .option('-r, --is-unsent <bool>', 'Filter messages by if they were unsent.')
+    .action(filter);
 
 program
     .command('collate')
     .description('Collate all message files into a single json.')
     .option('-o, --output <file>', 'Name of file to be outputed. If not specified, will default to \'collate.json\'')
     .action(collate);
-
-program
-    .command('fix-encoding')
-    .description(`Fixes Mojibake issue of messenger archive files. ${chalk.red('ONLY RUN ONCE.')} By default will go through every valid file.`)
-    .action(fixEncoding);
 
 // program
 //   .command('list')
